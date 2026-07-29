ea755bd3477f64b1949ee1652a8f3c904977b8b9
kdi1221 <ukdi1221@hanmail.net>
2026-07-29 23:24:40 +0900

    콘솔 게임 프로젝트 구성

---
+++ CraftEngine/Engine/Engine.cpp
@@ -0,0 +1,130 @@
+﻿#include "Engine.h"
+#include <Windows.h>
+#include <stdint.h>
+#include <iostream>
+#include <cassert>
+
+namespace Craft
+{
+	Engine* Engine::instance = nullptr;
+
+	Engine::Engine()
+	{
+		// 엔진 객체가 생성될때 이전에 만든 엔진 객체가 없어야 함
+		assert(!instance);
+		instance = this;
+	}
+
+	Engine::~Engine()
+	{
+		instance = nullptr;
+	}
+
+	void Engine::Run()
+	{
+		//윈도우 제공 고해상도 타이머(하드웨어 타이머).
+
+		//QueryPerformanceFrequency : 타이머의 해상도.
+		// ex : 밀리세컨드(1/1000)해상도 = 1000.
+		LARGE_INTEGER frequency;
+		QueryPerformanceFrequency(&frequency);
+
+		//현재 시간 확인
+		LARGE_INTEGER counter;
+		QueryPerformanceCounter(&counter);
+
+		// 프레임 시간 계산을 위한 변수.
+		int64_t currentTime = counter.QuadPart;
+		int64_t previousTime = currentTime;
+
+		// 프레임 고정.
+		const float oneFrameTime = 1.f / setting.framerate;
+
+		while(!isQuit)
+		{
+			// 입력 처리.
+			ProcessInput();
+
+			// 현재 시간 확인.
+			QueryPerformanceCounter(&counter);
+			currentTime = counter.QuadPart;
+
+			// 프레임 시간 계산 : (현재시간 - 이전시간) / 타이머 해상도
+			const float deltaTime = static_cast<float>(currentTime - previousTime) / static_cast<float>(frequency.QuadPart);
+
+			// 고정 프레임 처리
+			if (deltaTime >= oneFrameTime)
+			{
+				// 레벨 초기화 이벤트 함수.
+				OnInitialized();
+
+				// 레벨의 액터 초기화 이벤트 함수.
+				BeginPlay();
+
+				//레벨의 액터 업데이트 함수.
+				Tick(deltaTime);
+
+				// 업데이트된 결과를 화면에 그리는 함수. 
+				Draw();
+
+				//처리된 입력을 이전 프레임 입력으로 저장.
+				SavePreviousInputStates();
+
+				// 이전 프레임 시간 기록.
+				previousTime = currentTime;
+			}
+		} // while(true)
+
+		Shutdown();
+	}
+
+	void Engine::Quit()
+	{
+		// 종료 플래그 설정.
+		isQuit = true;
+	}
+
+	Engine& Engine::Get()
+	{
+		// Assert, 필수 조건 확인.
+		assert(instance);
+
+		// 전역 객체 반환.
+		return *instance;
+	}
+
+	void Engine::ProcessInput()
+	{
+
+	}
+
+	void Engine::OnInitialized()
+	{
+
+	}
+
+	void Engine::BeginPlay()
+	{
+
+	}
+
+	void Engine::Tick(float deltaTime)
+	{
+		std::cout << "DeltaTime: " << deltaTime << " | FPS: " << (1.f / deltaTime) << '\n';
+	}
+
+	void Engine::Draw()
+	{
+
+	}
+
+	void Engine::SavePreviousInputStates()
+	{
+
+	}
+
+	void Engine::Shutdown()
+	{
+
+	}
+}
---
+++ CraftEngine/Main.cpp
@@ -0,0 +1,9 @@
+﻿#include "Engine/Engine.h"
+
+using namespace Craft;
+
+int main(void)
+{
+	Engine engine;
+	engine.Run();
+}
